82e30d47e7e0dd7c38164c974fa671b83855eef0
captainfrak <saez.sylvain@gmail.com>
2019-11-25 16:02:37 +0100

    Add: create Trick entity

---
+++ src/Entity/Trick.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Entity;
+
+use DateTimeInterface;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TrickRepository")
+ *  * @UniqueEntity(fields={"name"}, message="Une figure avec le même nom existe déjà")
+ */
+class Trick
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=1000)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $figureGroup;
+
+    /**
+     * @ORM\Column(type="string", length=500, nullable=true)
+     */
+    private $illustration;
+
+    /**
+     * @ORM\Column(type="string", length=500, nullable=true)
+     */
+    private $trickVideo;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getFigureGroup(): ?string
+    {
+        return $this->figureGroup;
+    }
+
+    public function setFigureGroup(string $figureGroup): self
+    {
+        $this->figureGroup = $figureGroup;
+
+        return $this;
+    }
+
+    public function getIllustration(): ?string
+    {
+        return $this->illustration;
+    }
+
+    public function setIllustration(?string $illustration): self
+    {
+        $this->illustration = $illustration;
+
+        return $this;
+    }
+
+    public function getTrickVideo(): ?string
+    {
+        return $this->trickVideo;
+    }
+
+    public function setTrickVideo(?string $trickVideo): self
+    {
+        $this->trickVideo = $trickVideo;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
