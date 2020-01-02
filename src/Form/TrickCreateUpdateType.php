170bcc1dc34654586a161b3b72d1932b9935238d
captainfrak <saez.sylvain@gmail.com>
2020-01-02 08:59:17 +0100

    Add: change the type for CollectionType for the trickVideo field

--- src/Form/TrickCreateUpdateType.php
+++ src/Form/TrickCreateUpdateType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Trick;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,9 +18,28 @@ class TrickCreateUpdateType extends AbstractType
             ->add('name')
             ->add('description')
             ->add('figureGroup')
-            ->add('illustration')
-            ->add('trickVideo')
-        ;
+            /*->add('illustration', FileType::class, [
+                'label' => 'Selectionner votre image',
+                'mapped' => false,
+                'required' => false,
+            ])*/
+            ->add('trickPics', CollectionType::class, [
+                'entry_type' => TrickPicFormType::class,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+                'required' => false,
+                'prototype' => true
+            ])
+            ->add('trickVideos', CollectionType::class, [
+                'entry_type' => TextType::class,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+                'required' => false,
+                'prototype' => true
+            ])
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
